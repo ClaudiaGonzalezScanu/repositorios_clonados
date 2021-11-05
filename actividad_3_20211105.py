1d2e97d57d832c4597b80e9e2247c64dc9fddca2
ClaudiaG <claudia.gonzalezscanu@gmail.com>
2021-11-05 17:13:18 -0300

    Declara_actividades

---
+++ actividad_3_20211105.py
@@ -0,0 +1,19 @@
+###Evaluando si es un numero,letra, decimal
+#buscar tipos de datos en python, ¿cuales faltan?
+#agregar evaluación de dato y que devuelva tipo
+
+numero = 1
+letra = "bsdfsdf" 
+decimal = 2.5
+
+"""estoy consultando el tipo de dato es flotante?, sino imprimo el tipo de dato"""
+if type(letra) is float: 
+    print('es letra')
+else:
+    print(f'El tipo de dato es: {type(letra)}') #imprimo el tipo de dato
+
+"""estoy consultando el tipo de dato es string?, sino imprimo el tipo de dato"""
+if type(numero) is str:   
+    print('esto es un numero')  #imprimo el tipo de dato
+else: 
+    print(f'El tipo de dato es: {type(numero)}') 
